29a27566fbf394e00dfbeabf28a8462708b0a48d
gankston <gankston@users.noreply.github.com>
2026-05-11 18:04:47 -0300

    bump version 3.0.6 (versionCode 46)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.registro.empleados"
         minSdk = 26
         targetSdk = 35
-        versionCode = 45
-        versionName = "3.0.5"
+        versionCode = 46
+        versionName = "3.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
